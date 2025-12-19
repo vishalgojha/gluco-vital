f0d681a818fb7280c5815fce6b0b9b32cbc5741f
propai.bharat <propai.bharat@gmail.com>
2025-12-19 00:32:13 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -14,6 +14,7 @@ import Profile from './pages/Profile';
 import Progress from './pages/Progress';
 import Reports from './pages/Reports';
 import Terms from './pages/Terms';
+import AdminDashboard from './pages/AdminDashboard';
 import __Layout from './Layout.jsx';
 
 
@@ -34,6 +35,7 @@ export const PAGES = {
     "Progress": Progress,
     "Reports": Reports,
     "Terms": Terms,
+    "AdminDashboard": AdminDashboard,
 }
 
 export const pagesConfig = {
