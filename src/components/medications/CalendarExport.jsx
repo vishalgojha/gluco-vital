35e72985b92012353c1cf9ffa3d2857067f82286
support <support@glucovital.fit>
2025-12-21 12:52:39 +0000

    File changes

--- src/components/medications/CalendarExport.jsx
+++ src/components/medications/CalendarExport.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { Calendar, Download } from "lucide-react";
 import { format, addDays, setHours, setMinutes } from "date-fns";
